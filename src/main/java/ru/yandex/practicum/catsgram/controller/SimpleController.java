c2499a9f33e591099fc42c7e87c93a8d410e7b25
swwwer82 <swwwer82@yandex.ru>
2024-01-30 14:32:50 +0300

    add:new controller & model

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,12 +1,13 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String homePage() {
         return "Котограм";
     }
